26233ef812e3b0d95d3795e768bdd803430507c1
Pam Cairo <pcairo_90@outlook.com>
2019-09-28 22:30:37 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -30,4 +30,5 @@ it "contains a local variable called first_number that is assigned to a number" 
 it "contains a local variable called product that is assigned to the resuilt of multiplying first_number and second_number" do
   first_number = get_variable_from_file(' ./calculator.rb', "first_number")
   second_number = get_variable_from_file('./calculator.rb', "second_number")
-  product =
+  product = get_variable_from_file(' ./calculator.rb', "product")
+  
