5b51e767d8e5fde1bd09ac1201d502281a2f2f2b
Pam Cairo <pcairo_90@outlook.com>
2019-09-28 23:37:25 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,5 +4,6 @@ it "contains a local variable called first_number that is assigned to a number" 
   first_number= get_variable_from_file('./calculator.rb', "first_number")
   
    expect(first_number).to be_an(Integer).or be_a(Float)
+   expect(3).to be_an(Integer).or be_a(Float)
  end 
 
