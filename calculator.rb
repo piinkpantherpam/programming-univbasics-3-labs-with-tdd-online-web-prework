6cf855a1a70236ded5fcd4a0fd00495cadf7ae17
Pam Cairo <pcairo_90@outlook.com>
2019-09-28 21:25:22 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,4 @@
+describe "./calculator.rb" do 
 it "contains a local variable called first_number that is assigned to a number" do 
   first_number= get_variable_from_file('./calculator.rb', "first_number")
   
