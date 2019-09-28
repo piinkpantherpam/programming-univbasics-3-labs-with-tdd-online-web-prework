4bcc9d98b66b47ee41a501bbd07736cdb592375e
Pam Cairo <pcairo_90@outlook.com>
2019-09-28 22:16:20 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -19,4 +19,4 @@ it "contains a local variable called first_number that is assigned to a number" 
  
  expect(sum).to eq(first_number+second_number)
  end
- it "contains a local variable called difference that is assigned to the result of "
+ it "contains a local variable called difference that is assigned to the result of subtracting "
