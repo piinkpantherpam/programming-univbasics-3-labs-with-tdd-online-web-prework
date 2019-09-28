31e673cd5fd56f52cdb9e1eca1be1315e2005d5d
Pam Cairo <pcairo_90@outlook.com>
2019-09-28 21:57:36 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -13,4 +13,4 @@ it "contains a local variable called first_number that is assigned to a number" 
  end
  
  it "comtains a local variable called sum that is assigned to the result of adding first_number and second_number" do 
-   first_number = get_variable_from_file('./calculator.rb')
+   first_number = get_variable_from_file('./calculator.rb', "first_number")
