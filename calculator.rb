ad79fb18adcd77bf3a8de9cb836dbc4a819e4bf6
Pam Cairo <pcairo_90@outlook.com>
2019-09-28 21:32:32 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,4 +4,4 @@ it "contains a local variable called first_number that is assigned to a number" 
   
    expect(first_number).to be_an(Integer).or be_a(Float)
  end 
- it contains 
+ it "contains a local variable 
