ec2db87711e62cd726b1f52ad05be5c848c10a5e
Pam Cairo <pcairo_90@outlook.com>
2019-09-29 00:15:00 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,8 +4,5 @@ it "contains a local variable called first_number that is assigned to a number" 
   first_number= get_variable_from_file('./calculator.rb', "first_number")
   
    expect(first_number).to be_an(Integer).or be_a(Float)
-first_number = "5" 
-second_number = "2"
-"first_number * second_number" = "10"
  end
  
