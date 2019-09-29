13aa5c781496894a604bdbccc14d79b346f235c8
Pam Cairo <pcairo_90@outlook.com>
2019-09-29 00:27:08 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -7,13 +7,7 @@ it "contains a local variable called first_number that is assigned to a number" 
    
    first_number = "2"
  end
- describe "./calculator.rb" do 
   
-it "contains a local variable called first_number that is assigned to a number" do 
-  first_number= get_variable_from_file('./calculator.rb', "first_number")
-  
-   expect(first_number).to be_an(Integer).or be_a(Float)
- end 
  it "contains a local variable called second_number that is assigned to a number that isn't 0" do 
    second_number= get_variable_from_file('./calculator.rb', "second_number")
    
