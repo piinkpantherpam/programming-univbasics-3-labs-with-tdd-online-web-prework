7f20e051cd83f7efb4c746389b736037c4ed7293
Pam Cairo <pcairo_90@outlook.com>
2019-09-29 00:28:51 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -17,3 +17,12 @@ it "contains a local variable called first_number that is assigned to a number" 
    second_number = "4"
  end
  
+ it "contains a local variable called sum that is assigned to the result of adding first_number and second_number" do 
+   first_number = get_variable_from_file('./calculator.rb', "first_number")
+   second_number = get_variable_from_file('./calculator.rb', "second_number")
+   sum = get_variable_from_file('./calculator.rb', "sum")
+ 
+ expect(sum).to eq(first_number+second_number)
+ sum = (first_number + second_number)
+ end
+ 
